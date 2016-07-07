a93e5779f7ec51b922e4e665858a40170d2dd92a
miles-fitzsimons <miles.fitzsimons@gmail.com>
2016-07-08 11:18:19 +1200

    About image changes on click and submit form shows and hides on click of submit heading

---
+++ components/content.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class Content extends Component {
+
+	render() {
+		return (
+			<div>
+				<p>An old silent pond...
+
+				A frog jumps into the pond,
+
+				splash! Silence again.</p>
+			</div>
+		)
+	}
+
+}
+
+export default Content
---
+++ components/header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Header extends Component {
+
+	render() {
+		return (
+			<h1>WELCOME TO HAIKU LAND TESTING 123</h1>
+		)
+	}
+
+}
+
+export default Header
