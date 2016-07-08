f3de1f372fe5bd76bcba30f6b94a4b30aa75453d
miles-fitzsimons <miles.fitzsimons@gmail.com>
2016-07-08 14:02:50 +1200

    submit button greys out

--- components/submitForm.js
+++ components/submitForm.js
@@ -7,36 +7,58 @@ class SubmitForm extends Component {
 			this.state = {
 				count1: 0,
 				count2: 0,
-				count3: 0
+				count3: 0,
+				class1: 'incorrectCount',
+				class2: 'incorrectCount',
+				class3: 'incorrectCount'
 			}
 	}
 
 	handleChange(e) {
-		var id = 'count' + e.target.name
+		var name = e.target.name
+		var idCount = 'count' + name
+		var idClass = 'class' + name
 		var currentLine = e.target.value
 		var t = this
+		var correctCount
+		if(e.target.name == 1 || e.target.name == 3) {
+			correctCount = 5
+		}
+		else {
+			correctCount = 7
+		}
 		getSyllables(currentLine)
 			.then(function(res) {
-				t.setState({[id]: res})
+				if(res === correctCount) {
+					t.setState({[idClass]: 'correctCount'})
+				}
+				else {
+					t.setState({[idClass]: 'incorrectCount'})
+				}
+				t.setState({[idCount]: res})
 			})
 			.catch(function(err) {
 				console.log('Err::', err)
 			})
+			if(this.state.count1 === 5 && this.state.count2 === 7 && this.state.count3 === 5) {
+				document.getElementById('submitButton').disabled = false
+			}
+			else {
+				document.getElementById('submitButton').disabled = true
+			}
+			console.log('AA', this.state)
 	}
 
-
-
 	render() {
-		console.log(getSyllables)
 		return (
 			<div>
 				<input type="text" placeholder="Title" /><br/><br/>
 				<input type="text" placeholder="Author" /><br/><br/>
-				<span><input name="1" type="text" placeholder="5 syllables" onChange={this.handleChange.bind(this)} /> {this.state.count1} syllables</span><br/><br/>
-				<span><input name="2" type="text" placeholder="7 syllables" onChange={this.handleChange.bind(this)} /> {this.state.count2} syllables</span><br/><br/>
-				<span><input name="3" type="text" placeholder="5 syllables" onChange={this.handleChange.bind(this)} /> {this.state.count3} syllables</span><br/><br/>
+				<span className={this.state.class1}><input name="1" type="text" placeholder="5 syllables" onChange={this.handleChange.bind(this)} /> {this.state.count1} syllables</span><br/><br/>
+				<span className={this.state.class2}><input name="2" type="text" placeholder="7 syllables" onChange={this.handleChange.bind(this)} /> {this.state.count2} syllables</span><br/><br/>
+				<span className={this.state.class3}><input name="3" type="text" placeholder="5 syllables" onChange={this.handleChange.bind(this)} /> {this.state.count3} syllables</span><br/><br/>
 				
-				<button>Submit</button>
+				<button id="submitButton" disabled>Submit</button>
 			</div>
 		)
 	}
