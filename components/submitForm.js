71b174cc62ae677944b8422dbe64795806fdb782
miles-fitzsimons <miles.fitzsimons@gmail.com>
2016-07-08 12:21:09 +1200

    syllable counter on form inputs  working

--- components/submitForm.js
+++ components/submitForm.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react'
+import getSyllables from '../lib/getSyllables'
 
 class SubmitForm extends Component {
+		constructor (props) {
+	  super(props)
+			this.state = {
+				count1: 0,
+				count2: 0,
+				count3: 0
+			}
+	}
+
+	handleChange(e) {
+		var id = 'count' + e.target.name
+		var currentLine = e.target.value
+		var t = this
+		getSyllables(currentLine)
+			.then(function(res) {
+				t.setState({[id]: res})
+			})
+			.catch(function(err) {
+				console.log('Err::', err)
+			})
+	}
+
+
 
 	render() {
+		console.log(getSyllables)
 		return (
 			<div>
-				<input type="text" /><br/><br/>
-				<input type="text" /><br/><br/>
-				<input type="text" /><br/><br/>
+				<input type="text" placeholder="Title" /><br/><br/>
+				<input type="text" placeholder="Author" /><br/><br/>
+				<span><input name="1" type="text" placeholder="5 syllables" onChange={this.handleChange.bind(this)} /> {this.state.count1} syllables</span><br/><br/>
+				<span><input name="2" type="text" placeholder="7 syllables" onChange={this.handleChange.bind(this)} /> {this.state.count2} syllables</span><br/><br/>
+				<span><input name="3" type="text" placeholder="5 syllables" onChange={this.handleChange.bind(this)} /> {this.state.count3} syllables</span><br/><br/>
+				
 				<button>Submit</button>
 			</div>
 		)
